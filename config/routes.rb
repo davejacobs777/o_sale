e0433900a17864bc51965bb052a62bd0d1f4769d
David Jacobsen <dgrjacobsen@gmail.com>
2019-12-25 19:14:32 +0100

    Section 6: Reading Product Data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root "products#index"
+
+  get '/products/:id', to: 'products#show', as: 'product'
+  get '/products', to: 'products#index'
 end
+
+# $ rails routes -c products
