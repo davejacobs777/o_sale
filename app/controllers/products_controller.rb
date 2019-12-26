d7c09c86264f5bd65f9e4bf03525808c643af10f
David Jacobsen <dgrjacobsen@gmail.com>
2019-12-26 08:55:00 +0100

    Section 7: Creating and Editing products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,28 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to root_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to root_path
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :image_url)
+  end
 end
